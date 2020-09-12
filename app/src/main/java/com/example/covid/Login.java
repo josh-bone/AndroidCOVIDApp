ab8da2b5189c20f17d74e0ea3bc4d09caf12fa68
josh-bone <jbone@bu.edu>
2020-09-12 16:09:30 -0400

    Created Login and Register activities. New files: activity_login2.xml activity_register.xml Login Register

---
+++ app/src/main/java/com/example/covid/Login.java
@@ -0,0 +1,14 @@
+package com.example.covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+    //note, this corresponds to activity_login2 for now
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
