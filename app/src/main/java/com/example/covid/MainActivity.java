272372f6ac8338a40a07519784adedbfd2d4e52f
josh-bone <jbone@bu.edu>
2020-09-14 12:02:33 -0400

    Fixed Google Sign in - now located in main activity note: crashes after sign-in because the main app is not set up yet

--- app/src/main/java/com/example/covid/MainActivity.java
+++ app/src/main/java/com/example/covid/MainActivity.java
@@ -1,9 +1,27 @@
 package com.example.covid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -12,19 +30,86 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SignInButton mSignInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth fAuth;
+    private int SIGN_IN = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+
+        fAuth = FirebaseAuth.getInstance();
+        mSignInButton = findViewById(R.id.SignInBtn);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);*/
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mSignInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                signIn();
+            }
+        });
+    }
+
+    //this method gets called when the google sign-in button is clicked
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, SIGN_IN);
+    }
+
+    //here on is untested
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            //separate method will handle the result
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask){
+        try{
+            GoogleSignInAccount acc = completedTask.getResult(ApiException.class);
+            Authenticate(acc);
+        }
+        catch(ApiException ex){
+            Authenticate(null);
+        }
+    }
+
+    private void Authenticate(GoogleSignInAccount acc){
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(acc.getIdToken(), null);
+        fAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            //listens for task completion (whether successful or not, should complete)
+            //potential problem: what happens if task doesn't complete?
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = fAuth.getCurrentUser();
+                    updateUI(user);
+                }
+                else{
+                    updateUI(null);
+                }
+            }
+        });
+    }
+
+    private void updateUI(FirebaseUser fUser){
+        GoogleSignInAccount acc = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if(acc != null){
+            //user is signed in - go to app
+            //app currently crashes at this point because nothing is here
+            String userName = acc.getDisplayName();
+            Toast.makeText(MainActivity.this, userName, Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
