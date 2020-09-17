4d3eb2f8b6601c1387eda5c2e3ac9fe6376e2ca2
josh-bone <jbone@bu.edu>
2020-09-17 00:30:44 -0400

    Added a testcase for InfoDashActivity

--- app/src/androidTest/java/com/example/covid/InfoDashActivityTest.java
+++ app/src/androidTest/java/com/example/covid/InfoDashActivityTest.java
@@ -1,7 +1,21 @@
 package com.example.covid;
 
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 public class InfoDashActivityTest {
-    //TODO: add tests
+    @Test
+    public void test_textViewsAreDisplayed(){
+        ActivityScenario<AdminActivity> scenario = ActivityScenario.launch(AdminActivity.class);
+        onView(withId(R.id.totalConfirmed)).check(matches(isDisplayed()));
+        onView(withId(R.id.confirmedNumber)).check(matches(isDisplayed()));
+        onView(withId(R.id.dateText)).check(matches(isDisplayed()));
+    }
 }
